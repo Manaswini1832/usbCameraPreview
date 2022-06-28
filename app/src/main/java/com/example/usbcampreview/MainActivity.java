069e8cba1d44c5e929b01eaeb2f1e362df4d43eb
Manaswini <munugurimanaswini@gmail.com>
2022-06-28 10:29:39 +0530

    USB camera permission works!

--- app/src/main/java/com/example/usbcampreview/MainActivity.java
+++ app/src/main/java/com/example/usbcampreview/MainActivity.java
@@ -2,92 +2,105 @@ package com.example.usbcampreview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
-import android.hardware.usb.UsbDeviceConnection;
 import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "USB_CAMERA_APP_LOGS2";
-    private static String ACTION_USB_PERMISSION = "com.android.example.USB_PERMISSION";
+    private static final String TAG = "USB_CAMERA_APP_LOGS";
+    private static final String ACTION_USB_PERMISSION = "com.example.usbcampreview.USB_PERMISSION";
 
-    private BroadcastReceiver m_UsbReceiver = null;
-    private PendingIntent mPermissionIntent = null;
 
+    private Button btnGet;
+    private Intent intent = getIntent();
+
+    private UsbDevice device;
+
+    private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (ACTION_USB_PERMISSION.equals(action)) {
+                synchronized (this) {
+                    UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    //TODO : Make user accept camera permission. For now I did it deliberately in phone's app permission settings
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if(device != null){
+                            Log.d(TAG, "permission accepted for device");
+                        }
+                    }
+                    else {
+                        Log.d(TAG, "permission denied for device");
+                    }
+                }
+            }
+        }
+    };
+
+    @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(ACTION_USB_PERMISSION);
-        UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        UsbManager manager = (UsbManager) getSystemService(Context.USB_SERVICE);
 
-        mPermissionIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
-        IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
-        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        btnGet = findViewById(R.id.btnGet);
 
-        m_UsbReceiver = new BroadcastReceiver() {
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
+        btnGet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Get the USB camera object
+                device = enumerateUSBDevices(manager);
 
-                Log.d(TAG, "CAME TILL HEREEEEEEEEEEEEEEEEEEEEE");
-                if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(action)) {
-                    UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-                    if (device != null) {
-                        // call your method that cleans up and closes communication with the device
-                        Log.v("BroadcastReceiver", "Device Detached");
-                    }
-                }
-                if (ACTION_USB_PERMISSION.equals(action)) {
-                    synchronized (this) {
-                        UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                //Log info about the device : device name, product name, class, subclass, protocol
+                logDeviceInfo(device);
 
-                        if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
-                            Log.d(TAG, "------------------------------------------------------------------");
-                            if(device != null){
-                                //call method to set up device communication
-                                Log.d(TAG, "device permission granted ");
-                            }
-                        }
-                        else {
-                            Log.d(TAG, "permission denied for device ");
-                        }
-                        Log.d(TAG, "------------------------------------------------------------------");
-                    }
-                }
-
+                //Get user's permission to communicate with the USB device
+                PendingIntent mPermissionIntent = PendingIntent.getBroadcast(MainActivity.this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+                IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+                registerReceiver(mUsbReceiver, filter);
+                manager.requestPermission(device, mPermissionIntent);
             }
-        };
-
-
-        registerReceiver(m_UsbReceiver, filter);
-
-        HashMap<String, UsbDevice> deviceList = usbManager.getDeviceList();
-        Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
-        while(deviceIterator.hasNext()){
-            UsbDevice device = deviceIterator.next();
-            usbManager.requestPermission(device, mPermissionIntent);
-        }
+        });
 
     }
+
+
+    //TODO : Enumerates all connected USB devices but should only return the external camera
+    private UsbDevice enumerateUSBDevices(UsbManager manager){
+        HashMap<String, UsbDevice> deviceList = manager.getDeviceList();
+        Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
+        UsbDevice device = null;
+        while(deviceIterator.hasNext()){
+            device = deviceIterator.next();
+        }
+        return device;
+    }
+
+    @SuppressLint("NewApi")
+    private void logDeviceInfo(UsbDevice device){
+        Log.d(TAG, device.getDeviceName());
+        Log.d(TAG, device.getProductName());
+        Log.d(TAG, String.valueOf(device.getDeviceClass()));
+        Log.d(TAG, String.valueOf(device.getDeviceSubclass()));
+        Log.d(TAG, String.valueOf(device.getDeviceProtocol()));
+    }
 }
-
-
 
 
 
